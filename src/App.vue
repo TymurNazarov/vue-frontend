c578f53dac6684ccbd524965e0a89557a26757c3
Tymur Nazarov <tymur.nazarov@gmail.com>
2018-07-30 20:16:49 +0300

    init frontend

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  	<input type="text" v-model="input">
+  	<p :value="input"></p>
+  	<button v-on:click.prevent="request()">Request</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      input: '123'
+    }
+  },
+  methods: {
+  	request() {
+  		var data = {
+  			input: this.input
+  		}
+  		this.$http.post('http://laravel-backend/api/request', data)
+  		.then(res => {
+  			console.log(res);
+  		});
+  	}
+  }
+}
+</script>
+
+<style>
+
+</style>
